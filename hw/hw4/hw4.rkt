8fc1fe6c15e1eeb72a89aab2bc7f7b6e17a17036
Chao Wang <wcwxy@mail.ustc.edu.cn>
2021-04-22 12:18:01 +0800

    feat: add solution for hw 4, not include challenge problems

--- hw/hw4/hw4.rkt
+++ hw/hw4/hw4.rkt
@@ -4,4 +4,99 @@
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
 ;; put your code below
+; Problem 1
+(define (sequence spacing low high)
+  (if (> low high)
+      null
+      (cons low (sequence spacing (+ low spacing) high))))
 
+; Problem 2
+(define (string-append-map xs suffix)
+  (map
+   (lambda (a) (string-append a suffix))
+   xs))
+
+; Problem 3
+(define (list-nth-mod  xs n)
+  (if (< n 0)
+      (error "list-nth-mod: negative number")
+      (if (equal? xs null)
+          (error "list-nth-mod: empty list")
+          (let* ([len (length xs)]
+                 [rem (remainder n len)])
+            (car (list-tail xs rem))))))
+
+; Problem 4
+(define (stream-for-k-steps s k)
+  (if (= k 0)
+      null
+      (cons (car (s)) (stream-for-k-steps (cdr (s)) (- k 1)))))
+
+; Problem 5
+(define funny-number-stream
+  (letrec ([f (lambda (x) (cons
+                           (if (= 0 (remainder x 6))
+                               (- 0 x)
+                               x)
+                           (lambda () (f (+ x 1)))))])
+    (lambda () (f 1))))
+
+; Problem 6
+(define dan-then-dog
+  (letrec ([f (lambda (x) (cons x (lambda () (f (if (equal? x "dan.jpg") "dog.jpg" "dan.jpg")))))])
+    (lambda () (f "dan.jpg"))))
+
+; Problem 7
+(define (stream-add-one s)
+  (lambda () (cons
+              (cons 1 (car (s)))
+              (stream-add-one (cdr (s))))))
+
+; Problem 8
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (n) (cons
+                           (cons (list-nth-mod xs n) (list-nth-mod ys n))
+                           (lambda () (f (+ n 1)))))])
+    (lambda () (f 0))))
+
+; Problem 9
+(define (vector-assoc v vec)
+  (letrec ([helper (lambda (i)
+                     (if (< i (vector-length vec))
+                         (let ([vi (vector-ref vec i)])
+                           (if (pair? vi)
+                               (if (equal? v (car vi))
+                                   vi
+                                   (helper (+ i 1)))
+                               (helper (+ i 1))))
+                         #f))])
+    (helper 0)))
+
+; Problem 10
+(define (caching-assoc xs n)
+  (letrec ([cache (make-vector n #f)]
+           [pos 0]
+           [f (lambda (x)
+                (let ([ans (vector-assoc x cache)])
+                  (if ans
+                      ans
+                      (let ([new-ans (assoc x xs)])
+                        (begin
+                          (vector-set! cache pos new-ans)
+                          (set! pos (remainder (+ 1 pos) n))
+                          new-ans)))))])
+    f))
+
+; Problem 11
+(define-syntax while-greater
+  (syntax-rules (do)
+    [(while-greater e1 do e2)
+     (let ([r1 e1])
+       (letrec ([loop (lambda ()
+                        (if (< r1 e2)
+                            (loop)
+                            #t))])
+         (loop)))]))
+
+
+; Challenge Problem 12-13, not done
--- hw/hw4/hw4tests.rkt
+++ hw/hw4/hw4tests.rkt
@@ -51,14 +51,77 @@
 ; so you need to comment them out until you are ready.
 ; Add more tests as appropriate, of course.
 
+(define test11 (equal? (sequence 1 0 5) '(0 1 2 3 4 5)))
+(begin (display "test11 = ") (displayln test11))
+(define test12 (equal? (sequence 2 3 11) '(3 5 7 9 11)))
+(begin (display "test12 = ") (displayln test12))
+(define test13 (equal? (sequence 3 3 8) '(3 6)))
+(begin (display "test13 = ") (displayln test13))
+(define test14 (equal? (sequence 1 3 2) null))
+(begin (display "test14 = ") (displayln test14))
+
+(define test2 (equal?
+               (list "dan.jpg" "dog.jpg" "curry.jpg" "dog2.jpg")
+               (string-append-map 
+                (list "dan" "dog" "curry" "dog2") 
+                ".jpg")))
+(begin (display "test2 = ") (displayln test2))
+
+(define test3 (= 2 (list-nth-mod '(1 2 3 4 5) 6)))
+(begin (display "test3 = ") (displayln test3))
+;(list-nth-mod '(1 2 3 4 5) -1)
+;(list-nth-mod '() 1)
+
+(define ones (lambda () (cons 1 ones)))
+(define test4 (equal? (stream-for-k-steps ones 3)
+                      '(1 1 1)))
+(begin (display "test4 = ") (displayln test4))
+
+(define test5 (equal? (stream-for-k-steps funny-number-stream 16)
+                      '(1 2 3 4 5 -6 7 8 9 10 11 -12 13 14 15 16)))
+(begin (display "test5 = ") (displayln test5))
+
+(define test6 (equal? (stream-for-k-steps dan-then-dog 5)
+                      '("dan.jpg" "dog.jpg" "dan.jpg" "dog.jpg" "dan.jpg")))
+(begin (display "test6 = ") (displayln test6))
+
+(define test7 (equal? (stream-for-k-steps (stream-add-one dan-then-dog) 5)
+                      '((1 . "dan.jpg") (1 . "dog.jpg") (1 . "dan.jpg") (1 . "dog.jpg") (1 . "dan.jpg"))))
+(begin (display "test7 = ") (displayln test7))
+
+(define test8 (equal?
+               (stream-for-k-steps (cycle-lists '(1 2 3) '("a" "b")) 7)
+               '((1 . "a") (2 . "b") (3 . "a") (1 . "b") (2 . "a") (3 . "b") (1 . "a"))))
+(begin (display "test8 = ") (displayln test8))
+
+(define test91 (equal?
+                (vector-assoc 2 (vector '(1 2) 3 '(2 3))) '(2 3)))
+(begin (display "test91 = ") (displayln test91))
+(define test92 (equal?
+                (vector-assoc 3 (vector '(1 2) 3 '(2 3))) #f))
+(begin (display "test92 = ") (displayln test92))
+
+(define test10f (caching-assoc (list '(1 2) '(2 3) '(3 4)) 2))
+(define test101 (equal? (test10f 1) (assoc 1 (list '(1 2) '(2 3) '(3 4)))))
+(begin (display "test101 = ") (displayln test101))
+(define test102 (equal? (test10f 2) (assoc 2 (list '(1 2) '(2 3) '(3 4)))))
+(begin (display "test102 = ") (displayln test102))
+(define test103 (equal? (test10f 3) (assoc 3 (list '(1 2) '(2 3) '(3 4)))))
+(begin (display "test103 = ") (displayln test103))
+(define test104 (equal? (test10f 4) (assoc 4 (list '(1 2) '(2 3) '(3 4)))))
+(begin (display "test104 = ") (displayln test104))
+
+(define a 7)
+(define test111 (while-greater (begin (display "e2") 2) do (begin (set! a (- a 1)) (display "e1") a)))
+(display "\n")
+(define test112 (while-greater (begin (display "e2") 2) do (begin (set! a (- a 1)) (display "e1") a)))
+
+; origin tests
 (define nums (sequence 1 0 5))
 
 (define files (string-append-map 
                (list "dan" "dog" "curry" "dog2") 
                ".jpg"))
-
-(define funny-test (stream-for-k-steps funny-number-stream 16))
-
 ; a zero-argument function: call (one-visual-test) to open the graphics window, etc.
 (define (one-visual-test)
   (place-repeatedly (open-window) 0.5 (cycle-lists nums files) 27))
