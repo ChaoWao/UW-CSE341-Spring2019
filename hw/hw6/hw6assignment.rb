0e10a2e32342aa24a1315101ded09bb4d82c4dea
Chao Wang <wcwxy@mail.ustc.edu.cn>
2021-05-02 17:35:36 +0800

    feat: add solution for hw 6, not include challenge problems

--- hw/hw6/hw6assignment.rb
+++ hw/hw6/hw6assignment.rb
@@ -5,19 +5,77 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-
+  All_My_Pieces = All_Pieces + [
+    rotations([[0, 0], [1, 0], [0, 1], [-1, 1], [-1, 0]]),
+    [[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
+     [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
+    rotations([[0, 0], [1, 0], [0, 1]])
+  ]
+  
   # your enhancements here
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
 
+  def self.cheat_piece (board)
+    MyPiece.new([[[0, 0]]], board)
+  end
+  
 end
 
 class MyBoard < Board
   # your enhancements here
+  def initialize (game)
+    super
+    @current_block = MyPiece.next_piece(self)
+    @cheat = false
+  end
 
+  def next_piece
+    if @cheat
+      @current_block = MyPiece.cheat_piece(self)
+      @cheat = false
+    else
+      @current_block = MyPiece.next_piece(self)
+    end
+    @current_pos = nil
+  end
+
+  def set_cheat
+    if score >= 100 and !@cheat
+      @cheat = true
+      @score -= 100
+    end
+  end
+  
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0..(locations.size - 1 )).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
+  
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
 
+  def key_bindings
+    super
+    @root.bind('u', proc {2.times {@board.rotate_clockwise}})
+    @root.bind('c', proc {@board.set_cheat})
+  end
+  
 end
-
-
